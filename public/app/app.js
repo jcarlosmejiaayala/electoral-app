239398998ddda6363748202c3a2e0beae30ad228
jcarlosmejiaayala <jcarlosmejiaayala@gmail.com>
2015-01-26 17:43:09 -0600

    electoral-app

--- public/app/app.js
+++ public/app/app.js
@@ -41,8 +41,7 @@
             'ui.router',
             'highcharts-ng',
             'ui.bootstrap',
-            'oitozero.ngSweetAlert',
-            'slick'
+            'oitozero.ngSweetAlert'
         ])
         .config(config)
         .factory('authInterceptor', factory)
--- public/app/main/main.controller.js
+++ public/app/main/main.controller.js
@@ -2,17 +2,6 @@
     'use strict';
 
     function controller(usuario) {
-        this.slideImages = [
-            {
-                image: 'vote.jpg',
-                message: 'Este es un texto de ejemplo',
-                link: '/suscribirse'
-            },
-            {
-                image: 'vote.jpg',
-                message: 'Este es un texto de ejemplo'
-            }];
-        this.intervalCarousel = 5000;
     }
 
     angular
