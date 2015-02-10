f83ae727bb3c226a7965722f3013ba12b7b122d1
jcarlosmejiaayala <jcarlosmejiaayala@gmail.com>
2015-02-10 17:01:16 -0600

    electoral-app

--- public/controllers/registro.controller.js
+++ public/controllers/registro.controller.js
@@ -3,11 +3,23 @@
 var controller = function (estados, SweetAlert) {
     var that = this;
     this.form = {};
-    this.estados = estados
+    angular.extend(this, {
+        partidos: ['pri', 'pan', 'prd', 'pt', 'morena', 'ind', 'pve', 'mc', 'panal'],
+        candidaturas: ['Alcaldia', 'Diputación Local', 'Diputación Federal', 'Gobernatura', 'Presidencia Nacional']
+    });
+    angular.extend(this.form, {
+        telefonos: {},
+        redesSociales: {},
+        partido: this.partidos[0],
+        candidatura: this.candidaturas[0],
+        status: true,
+        rol: 'candidato'
+    });
+    estados
         .get()
         .then(function (data) {
             that.estados = data;
-            that.form.estados = data[0];
+            that.form.estado = data[0];
         }, function (err) {
             SweetAlert
                 .swal({
