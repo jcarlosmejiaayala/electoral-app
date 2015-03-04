c151991f1fa13d562c948b006dbee45424cf278c
jcarlosmejiaayala <jcarlosmejiaayala@gmail.com>
2015-03-04 11:47:28 -0600

    'electoral'

---
+++ public/bower_components/angular-material/modules/js/whiteframe/whiteframe.js
@@ -0,0 +1,15 @@
+/*!
+ * Angular Material Design
+ * https://github.com/angular/material
+ * @license MIT
+ * v0.7.0-rc1-master-84842ff
+ */
+(function() {
+'use strict';
+
+/**
+ * @ngdoc module
+ * @name material.components.whiteframe
+ */
+angular.module('material.components.whiteframe', []);
+})();
