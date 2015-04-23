a384c74b2c34cd3c0b73388b4f2cb9a3f3b34779
jcarlosmejiaayala <jcarlosmejiaayala@gmail.com>
2015-04-23 13:29:17 -0500

    'electoral'

--- app/config/enviroment/production.js
+++ app/config/enviroment/production.js
@@ -3,8 +3,6 @@
 exports.mongo = {
     uri: 'mongodb://localhost:27017/electoral',
     options: {
-        server: {socketOptions: {keepAlive: 1, connectTimeoutMS: 30000}},
-        replset: {socketOptions: {keepAlive: 1, connectTimeoutMS: 30000}},
         db: {
             safe: true
         }
